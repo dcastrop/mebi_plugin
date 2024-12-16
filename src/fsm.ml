bb823a446b7bb6fd169e7dde6ef98ab03652cc22
Jonah Pears <jonahpears@gmail.com>
2024-12-16 18:55:23 +0000

    updating fsm

--- src/fsm.ml
+++ src/fsm.ml
@@ -29,6 +29,18 @@ module States = Set.Make (struct
     let compare a b = compare a.id b.id
   end)
 
+(*************************************************)
+(****** Blocks & Partitions of States ************)
+(****** (Used by Bisimilarity algorithms.) *******)
+(****** (Featured here for more seemless pstr.) **)
+(*************************************************)
+
+(** [Block] is a set of states.
+    This is necessary since [KS90] requires the actions of states to be sortable. *)
+module Block = States
+
+module Partition = Set.Make (States)
+
 (*********************************************************************)
 (****** Action Labels & Alphabet *************************************)
 (*********************************************************************)
@@ -134,6 +146,8 @@ type pp_axiom =
 type pp_list =
   | States of States.t
   | Alphabet of Alphabet.t
+  | Block of Block.t
+  | Partition of Partition.t
 
 type pp_map =
   | Actions of States.t Actions.t
@@ -163,14 +177,16 @@ type pp_wrappable =
   | Edge of (state * action * state)
   | OutgoingEdge of (action * state)
   | States of States.t
+  | Block of Block.t
   | Alphabet of Alphabet.t
   | Actions of States.t Actions.t
   | Edges of States.t Actions.t Edges.t
+  | Partition of Partition.t
   | Fsm of fsm
 
 (** [pp_options] denotes the types that specifically support the [pp] flag. *)
 type pp_options =
-  | None of unit
+  | Default of unit
   | Debug of unit
 
 (** [pp_collection_is_empty c] returns true if [c] is empty. *)
@@ -179,7 +195,13 @@ let pp_collection_is_empty (c : pp_collection) : bool =
   | List l ->
     (match l with
      | States s -> States.is_empty s
-     | Alphabet a -> Alphabet.is_empty a)
+     | Alphabet a -> Alphabet.is_empty a
+     | Block b -> Block.is_empty b
+     | Partition p ->
+       (* check also that any blocks are empty too *)
+       if Partition.is_empty p
+       then true
+       else Partition.for_all (fun (b : Block.t) -> Block.is_empty b) p)
   | Map m ->
     (match m with
      | Actions a -> Actions.length a == 0
@@ -194,21 +216,23 @@ let pp_wrap_as_supported (to_wrap : pp_wrappable) : pp_supported =
   | Edge to_wrap -> Axiom (Edge to_wrap)
   | OutgoingEdge to_wrap -> Axiom (OutgoingEdge to_wrap)
   | States to_wrap -> Collection (List (States to_wrap))
+  | Block to_wrap -> Collection (List (Block to_wrap))
   | Alphabet to_wrap -> Collection (List (Alphabet to_wrap))
   | Actions to_wrap -> Collection (Map (Actions to_wrap))
   | Edges to_wrap -> Collection (Map (Edges to_wrap))
+  | Partition to_wrap -> Collection (List (Partition to_wrap))
   | Fsm to_wrap -> Fsm to_wrap
 ;;
 
 (** [pstr] *)
 let rec pstr
   ?(tabs : int = 0)
-  ?(options : pp_options = None ())
+  ?(options : pp_options = Default ())
   (to_str : pp_supported)
   : string
   =
-  let basedent = Mebi_utils.str_tabs (tabs - 1) in
-  let indent = Mebi_utils.str_tabs tabs in
+  let basedent = Utils.str_tabs (tabs - 1) in
+  let indent = Utils.str_tabs tabs in
   (* build string *)
   match to_str with
   (*  *)
@@ -223,7 +247,7 @@ let rec pstr
            "(%s%s)"
            to_str.pp
            (match options with
-            | None () -> ""
+            | Default () -> ""
             | Debug () -> Printf.sprintf " <id: %d>" to_str.id)
        (* [action] *)
        | Action to_str ->
@@ -231,7 +255,7 @@ let rec pstr
            "{| %s%s |}"
            to_str.label
            (match options with
-            | None () -> ""
+            | Default () -> ""
             | Debug () -> Printf.sprintf " <id: %d>" to_str.id)
        (* edge of [state * action * state] *)
        | OutgoingEdge to_str ->
@@ -275,6 +299,9 @@ let rec pstr
                        (pp_wrap_as_supported (State s))))
                 to_str
                 "\n"
+            (* [Block] *)
+            | Block to_str ->
+              pstr ~tabs ~options (pp_wrap_as_supported (States to_str))
             (* [Alphabet] *)
             | Alphabet to_str ->
               Alphabet.fold
@@ -286,6 +313,19 @@ let rec pstr
                        ~tabs:(tabs + 1)
                        ~options
                        (pp_wrap_as_supported (Action a))))
+                to_str
+                "\n"
+            (* [Partition] *)
+            | Partition to_str ->
+              Partition.fold
+                (fun (b : Block.t) (acc : string) ->
+                  Printf.sprintf
+                    "%s%s\n"
+                    acc
+                    (pstr
+                       ~tabs:(tabs + 1)
+                       ~options
+                       (pp_wrap_as_supported (Block b))))
                 to_str
                 "\n")
            basedent
@@ -355,10 +395,10 @@ let rec pstr
                 "\n")
            basedent)
   (*  *)
-  | Utils to_str -> "<<<<unimplemented>>>>"
+  | Utils _to_str -> "<<<<unimplemented>>>>"
   (*  *)
   | Fsm to_str ->
-    let indent = Mebi_utils.str_tabs (tabs + 1) in
+    let indent = Utils.str_tabs (tabs + 1) in
     Printf.sprintf
       "{ %s; %s; %s; %s; \n%s}"
       (Printf.sprintf
@@ -387,400 +427,6 @@ let rec pstr
             ~options
             (pp_wrap_as_supported (Edges to_str.edges))))
       basedent
-;;
-
-(** [pstr_state ?long state] is a string of [state].
-    [?ids] determines if [state.id] is shown.
-    [?pp] determins if [state.pp] is shown.
-    [?long] overrides the other options, showing everything.
-    By default only [state.id] is shown.
-    [state] is the state to be printed. *)
-let pstr_state
-  ?(ids : unit option)
-  ?(pp : unit option)
-  ?(long : unit option)
-  (state : state)
-  : string
-  =
-  match long with
-  | None ->
-    Printf.sprintf
-      "(%s)"
-      (match pp with
-       (* show [state.id] only *)
-       | None -> Printf.sprintf "id: %d" state.id
-       (* show [state.pp] *)
-       | Some () ->
-         Printf.sprintf
-           "%s%s"
-           state.pp
-           (* check if [show.id] too *)
-           (match ids with
-            | None -> ""
-            | Some () -> Printf.sprintf " <id: %d>" state.id))
-  (* show everything *)
-  | Some () -> Printf.sprintf "(id: %d; pp: %s)" state.id state.pp
-;;
-
-(* Printf.sprintf "(id: %d; hash: %d; pp: %s)" state.id state.hash state.pp *)
-
-(** [handle_state_pstr ids pp long s] is a wrapper for [pstr_state] which
-    makes it easier to pass the options from higher-level [pstr_] functions. *)
-let handle_state_pstr
-  (ids : unit option)
-  (pp : unit option)
-  (long : unit option)
-  (s : state)
-  : string
-  =
-  match long with
-  | None ->
-    (match pp with
-     | None -> pstr_state s
-     | Some () ->
-       (match ids with
-        | None -> pstr_state ~pp:() s
-        | Some () -> pstr_state ~ids:() ~pp:() s))
-  | Some () -> pstr_state ~long:() s
-;;
-
-(** [pstr_states ?long states] is a string of [states].
-    [?ids], [?pp] and [?long] are used for individual [states].
-    [?indent] determines the level of indentation.
-    [states] is the set of states to be printed. *)
-let pstr_states
-  ?(ids : unit option)
-  ?(pp : unit option)
-  ?(long : unit option)
-  ?(indent : int = 1)
-  (states : States.t)
-  : string
-  =
-  if States.is_empty states
-  then "[ ] (empty)"
-  else
-    Printf.sprintf
-      "[%s%s]"
-      (States.fold
-         (fun (s : state) (acc : string) ->
-           Printf.sprintf
-             "%s%s%s\n"
-             acc
-             (Mebi_utils.str_tabs indent)
-             (* figure out which units to pass on (bit messy) *)
-             (handle_state_pstr ids pp long s))
-         states
-         "\n")
-      (Mebi_utils.str_tabs (indent - 1))
-;;
-
-(** [pstr_action ?ids ?long action] is a string of [action].
-    [?ids] determines if [action.id] is shown.
-    [?long] determines if all fields are shown.
-    [action] is the action to show. *)
-let pstr_action ?(ids : unit option) ?(long : unit option) (action : action)
-  : string
-  =
-  match long with
-  | None ->
-    Printf.sprintf
-      "%s"
-      (Printf.sprintf
-         "%s%s"
-         action.label
-         (match ids with
-          | None -> ""
-          | Some () -> Printf.sprintf " <id: %d>" action.id))
-  (* show everything *)
-  | Some () -> Printf.sprintf "id: %d; label: %s" action.id action.label
-;;
-
-let handle_action_pstr
-  (ids : unit option)
-  (pp : unit option)
-  (long : unit option)
-  (a : action)
-  : string
-  =
-  match long with
-  | None ->
-    (match ids with
-     | None -> pstr_action a
-     | Some () -> pstr_action ~ids:() a)
-  | Some () -> pstr_action ~long:() a
-;;
-
-(** [pstr_action_alphabet ?ids ?long action] is a string of [action].
-    [?ids] determines if [action.id] is shown.
-    [?long] determines if all fields are shown.
-    [action] is the action to show. *)
-let pstr_action_alphabet
-  ?(ids : unit option)
-  ?(long : unit option)
-  ?(indent : int = 1)
-  (actions : Alphabet.t)
-  : string
-  =
-  if Alphabet.is_empty actions
-  then "[ ] (empty)"
-  else
-    Printf.sprintf
-      "[%s%s]"
-      (Alphabet.fold
-         (fun (a : action) (acc : string) ->
-           Printf.sprintf
-             "%s%s%s\n"
-             acc
-             (Mebi_utils.str_tabs indent)
-             (* figure out which units to pass on (bit messy) *)
-             (handle_action_pstr ids None long a))
-         actions
-         "\n")
-      (Mebi_utils.str_tabs (indent - 1))
-;;
-
-(** [pstr_edge ?long (from_state, outgoing_edge)] is a string of [edge].
-    [?ids], [?pp] and [?long] are used for corresponding states.
-    [from_state] is the starting state.
-    [outgoing_edge] denotes the edge label [outgoing_edge.label]
-    and the destination [outgoing_edge.to_state]. *)
-let pstr_edge
-  ?(ids : unit option)
-  ?(pp : unit option)
-  ?(long : unit option)
-  ((from_state : state), (action : action), (destination : state))
-  : string
-  =
-  Printf.sprintf
-    "%s ---{ %s }--> %s"
-    (handle_state_pstr ids pp long from_state)
-    (handle_action_pstr ids pp long action)
-    (handle_state_pstr ids pp long destination)
-;;
-
-(* TODO: merge below with [handle_state_pstr] ? *)
-(** [handle_edge_pstr ids pp long e] is a wrapper for [pstr_edge] which
-    makes it easier to pass the options from higher-level [pstr_] functions. *)
-let handle_edge_pstr
-  (ids : unit option)
-  (pp : unit option)
-  (long : unit option)
-  (e : state * action * state)
-  : string
-  =
-  match long with
-  | None ->
-    (match pp with
-     | None -> pstr_edge e
-     | Some () ->
-       (match ids with
-        | None -> pstr_edge ~pp:() e
-        | Some () -> pstr_edge ~ids:() ~pp:() e))
-  | Some () -> pstr_edge ~long:() e
-;;
-
-let pstr_actions
-  ?(ids : unit option)
-  ?(pp : unit option)
-  ?(long : unit option)
-  ?(indent : int = 1)
-  ?(from_state : state = { id = -1; pp = ".." })
-  (actions : States.t Actions.t)
-  : string
-  =
-  if List.is_empty (List.of_seq (Actions.to_seq_keys actions))
-  then "[ ] (empty)"
-  else
-    Printf.sprintf
-      "[%s%s]"
-      (Actions.fold
-         (fun (action : action) (destinations : States.t) (acc : string) ->
-           Printf.sprintf
-             "%s%s"
-             acc
-             (States.fold
-                (fun (destination : state) (acc' : string) ->
-                  Printf.sprintf
-                    "%s%s{ %s }\n"
-                    acc'
-                    (Mebi_utils.str_tabs indent)
-                    (handle_edge_pstr
-                       ids
-                       pp
-                       long
-                       (from_state, action, destination)))
-                destinations
-                ""))
-         actions
-         "\n")
-      (Mebi_utils.str_tabs (indent - 1))
-;;
-
-let handle_actions_pstr
-  ?(indent : int = 1)
-  ?(from_state : state = { id = -1; pp = ".." })
-  (ids : unit option)
-  (pp : unit option)
-  (long : unit option)
-  (a : States.t Actions.t)
-  : string
-  =
-  match long with
-  | None ->
-    (match ids with
-     | None ->
-       (match pp with
-        | None -> pstr_actions ~indent ~from_state a
-        | Some () -> pstr_actions ~pp:() ~indent ~from_state a)
-     | Some () ->
-       (match pp with
-        | None -> pstr_actions ~ids:() ~indent ~from_state a
-        | Some () -> pstr_actions ~ids:() ~pp:() ~indent ~from_state a))
-  | Some () -> pstr_actions ~long:() ~indent ~from_state a
-;;
-
-(* TODO: trying to combine all of the [handle_state_pstr], [handle_edge_pstr], etc. *)
-(** [handle_units] denotes the types that [handle_units_pstr] supports. *)
-(* type handle_units =
-   | State of state
-   | States of States.t
-   | Edge of fsm_transition
-   | Edges of edges *)
-
-(* let handle_units_pstr
-   (ids : unit option)
-   (pp : unit option)
-   (long : unit option)
-   (to_handle : handle_units)
-   (with_function) : string =
-
-   match long with
-   | None ->
-   (match pp with
-   | None -> with_function to_handle
-   | Some () ->
-   (match ids with
-   | None -> with_function ~pp:() to_handle
-   | Some () -> with_function ~ids:() ~pp:() to_handle))
-   | Some () -> with_function ~long:() to_handle
-   ;; *)
-
-(** [pstr_edges ?long edges] is a string of [edges].
-    [?ids], [?pp] and [?long] are used by individual edges.
-    [edges] is the list of edges to be printed. *)
-let pstr_edges
-  ?(ids : unit option)
-  ?(pp : unit option)
-  ?(long : unit option)
-  ?(indent : int = 1)
-  (edges : States.t Actions.t Edges.t)
-  : string
-  =
-  if Edges.to_seq_keys edges |> Seq.is_empty
-  then "[ ] (empty)"
-  else
-    Printf.sprintf
-      "[%s%s]"
-      (Edges.fold
-         (fun (from_state : state)
-           (outgoing_edges : States.t Actions.t)
-           (acc : string) ->
-           Printf.sprintf
-             "%s%s%s\n"
-             acc
-             (Mebi_utils.str_tabs indent)
-             (handle_actions_pstr
-                ~indent:(indent + 1)
-                ~from_state
-                ids
-                pp
-                long
-                outgoing_edges))
-         edges
-         "\n")
-      (Mebi_utils.str_tabs (indent - 1))
-;;
-
-(** [handle_states_pstr ids pp long e] is a wrapper for [pstr_states] which
-    makes it easier to pass the options from higher-level [pstr_] functions. *)
-let handle_states_pstr
-  ?(indent : int = 1)
-  (ids : unit option)
-  (pp : unit option)
-  (long : unit option)
-  (s : States.t)
-  : string
-  =
-  match long with
-  | None ->
-    (match pp with
-     | None -> pstr_states ~indent s
-     | Some () ->
-       (match ids with
-        | None -> pstr_states ~pp:() ~indent s
-        | Some () -> pstr_states ~ids:() ~pp:() ~indent s))
-  | Some () -> pstr_states ~long:() ~indent s
-;;
-
-(** [handle_pstr_alphabet ids pp long a] is a wrapper for [pstr_states] which
-    makes it easier to pass the options from higher-level [pstr_] functions. *)
-let handle_action_alphabet_pstr
-  (ids : unit option)
-  (pp : unit option)
-  (long : unit option)
-  (a : Alphabet.t)
-  : string
-  =
-  match long with
-  | None ->
-    (match ids with
-     | None -> pstr_action_alphabet a
-     | Some () -> pstr_action_alphabet ~ids:() a)
-  | Some () -> pstr_action_alphabet ~long:() a
-;;
-
-(** [handle_edges_pstr ids pp long e] is a wrapper for [pstr_edges] which
-    makes it easier to pass the options from higher-level [pstr_] functions. *)
-let handle_edges_pstr
-  (ids : unit option)
-  (pp : unit option)
-  (long : unit option)
-  (e : States.t Actions.t Edges.t)
-  : string
-  =
-  match long with
-  | None ->
-    (match pp with
-     | None -> pstr_edges e
-     | Some () ->
-       (match ids with
-        | None -> pstr_edges ~pp:() e
-        | Some () -> pstr_edges ~ids:() ~pp:() e))
-  | Some () -> pstr_edges ~long:() e
-;;
-
-(** [pstr_fsm ?long the_fsm] is a string of [the_fsm].
-    [?long] is used when printing individual states and edges.
-    [the_fsm] is the fsm to be printed. *)
-let pstr_fsm
-  ?(ids : unit option)
-  ?(pp : unit option)
-  ?(long : unit option)
-  (the_fsm : fsm)
-  : string
-  =
-  Printf.sprintf
-    "%s\n%s\n%s\n%s"
-    (Printf.sprintf
-       "init state: %s"
-       (handle_state_pstr ids pp long the_fsm.init))
-    (Printf.sprintf
-       "states: %s"
-       (handle_states_pstr ids pp long the_fsm.states))
-    (Printf.sprintf
-       "alphabet: %s"
-       (handle_action_alphabet_pstr ids pp long the_fsm.alphabet))
-    (Printf.sprintf "edges: %s" (handle_edges_pstr ids pp long the_fsm.edges))
 ;;
 
 (*********************************************************************)
@@ -829,14 +475,14 @@ let get_action_alphabet_from_edges (es : States.t Actions.t Edges.t)
   =
   Alphabet.of_list
     (Edges.fold
-       (fun (from_state : state)
+       (fun (_from_state : state)
          (actions : States.t Actions.t)
          (acc : action list) ->
          List.append
            acc
            (Actions.fold
               (fun (action : action)
-                (destinations : States.t)
+                (_destinations : States.t)
                 (acc' : action list) -> List.append acc' [ action ])
               actions
               []))
@@ -883,7 +529,7 @@ let get_action_by_label (alphabet : Alphabet.t) (label : string) : action =
 let get_outgoing_actions
   (edges : States.t Actions.t Edges.t)
   (from : state)
-  (a : action)
+  (_a : action)
   : States.t Actions.t
   =
   match Edges.find_opt edges from with
