e830996f3b3491df6e4f549de29dd290453e3d57
Jonah Pears <jonahpears@gmail.com>
2025-01-14 15:05:33 +0000

    fixed printout format

--- src/tests.ml
+++ src/tests.ml
@@ -8,11 +8,11 @@ let rec ks90_exas (exas : RCP.Examples.example list) : unit =
   | exa :: exas' ->
     (match exa with
      | { name; s; t; _ } ->
-       Printf.printf "\n= = = = = = = = = =\nRCP.KS90 (Exa2)\n";
-       (* Printf.printf "%s.s: %s" name (pstr (Fsm s)); *)
-       Printf.printf "%s.s: %s.\n" name (pstr ~options:(Debug ()) (Fsm s));
-       (* Printf.printf "%s.t: %s" name (pstr (Fsm t)); *)
-       Printf.printf "%s.t: %s.\n" name (pstr ~options:(Debug ()) (Fsm t));
+       Printf.printf "\n= = = = = = = = = =\nRCP.KS90 (%s)\n\n" name;
+       Printf.printf "%s.s: %s.\n" name (pstr (Fsm s));
+       Printf.printf "%s.t: %s.\n" name (pstr (Fsm t));
+       (* Printf.printf "%s.s: %s.\n" name (pstr ~options:(Debug ()) (Fsm s));
+          Printf.printf "%s.t: %s.\n" name (pstr ~options:(Debug ()) (Fsm t)); *)
        (* run algorithm *)
        let are_bisimilar, pi = RCP.KS90.run s t in
        (* print out results *)
@@ -26,13 +26,13 @@ let rec ks90_exas (exas : RCP.Examples.example list) : unit =
               "\nnon-bisimilar partition: %s."
               (pstr (pp_wrap_as_supported (Partition pi)))
           else "");
-       Printf.printf
-         "where s = %s\nand t = %s.\n"
-         (pstr (pp_wrap_as_supported (Fsm s)))
-         (pstr (pp_wrap_as_supported (Fsm t)));
-       Printf.printf
-         "\n--------\npi: %s.\n"
-         (pstr (pp_wrap_as_supported (Partition pi)));
+       (* Printf.printf
+          "where s = %s\nand t = %s.\n"
+          (pstr (pp_wrap_as_supported (Fsm s)))
+          (pstr (pp_wrap_as_supported (Fsm t))); *)
+       (* Printf.printf
+          "\n--------\npi: %s.\n"
+          (pstr (pp_wrap_as_supported (Partition pi))); *)
        Printf.printf "\n= = = = = = = = = =\n";
        ();
        (* continue *)
