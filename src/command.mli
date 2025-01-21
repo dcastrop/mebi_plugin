a35cc9c3d864efa3faa05d922a59557363eb90b2
Jonah Pears <jonahpears@gmail.com>
2025-01-21 12:23:18 +0000

    refreshed commands

--- src/command.mli
+++ src/command.mli
@@ -1,13 +1,27 @@
-val bounded_lts
-  :  ?debug:bool
+val cmd_bounded_lts
+  :  ?show_details:bool
+  -> ?show_debug:bool
   -> Names.GlobRef.t
   -> Constrexpr.constr_expr_r CAst.t
   -> unit Mebi_monad.t
 
-val fsm_bisim_ks90 : ?debug:bool -> Fsm.fsm -> Fsm.fsm -> unit Mebi_monad.t
+val cmd_bounded_lts_to_fsm
+  :  ?show_details:bool
+  -> ?show_debug:bool
+  -> Names.GlobRef.t
+  -> Constrexpr.constr_expr_r CAst.t
+  -> unit Mebi_monad.t
 
-val lts_bisim_ks90
-  :  ?debug:bool
+val cmd_bisim_ks90_using_fsm
+  :  ?show_details:bool
+  -> ?show_debug:bool
+  -> Fsm.fsm
+  -> Fsm.fsm
+  -> unit Mebi_monad.t
+
+val cmd_bisim_ks90_using_lts_to_fsm
+  :  ?show_details:bool
+  -> ?show_debug:bool
   -> Names.GlobRef.t * Constrexpr.constr_expr_r CAst.t
   -> Names.GlobRef.t * Constrexpr.constr_expr_r CAst.t
   -> unit Mebi_monad.t
