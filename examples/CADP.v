37b8ab62b1d6f84704fbc0d52cc4b113c0907f49
Jonah Pears <jonahpears@gmail.com>
2025-04-17 14:11:35 +0100

    updated comments

--- examples/CADP.v
+++ examples/CADP.v
@@ -894,7 +894,7 @@ Example p0 : tm * env := (P, Env.initial 1).
 Compute p0.
 
 (* MeBi Show  LTS Of p0 Using step. *)
-MeBi Dump "p0" LTS Bounded 150 Of p0 Using step.
+(* MeBi Dump "p0" LTS Bounded 150 Of p0 Using step. *)
 (* MeBi Debug LTS Of p0 Using step. *)
 
 (* MeBi Show  FSM Of p0 Using step. *)
@@ -951,7 +951,7 @@ Example ncs1 : sys * resource := compose (create 1 P).
 Compute ncs1.
 
 (* MeBi Show  LTS Of ncs1 Using lts step. *)
-MeBi Dump "NCS1" LTS Bounded 1000 Of ncs1 Using lts step.
+(* MeBi Dump "NCS1" LTS Bounded 1000 Of ncs1 Using lts step. *)
 (* MeBi Debug LTS Of ncs1 Using lts step. *)
 
 (* MeBi Show  FSM Of ncs1 Using lts step. *)
