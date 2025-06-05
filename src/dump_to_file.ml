169e2b5525d79499e862a777affc1747923aacd0
Jonah Pears <jonahpears@gmail.com>
2025-06-05 15:40:21 +0100

    testing coq memory model

--- src/dump_to_file.ml
+++ src/dump_to_file.ml
@@ -21,18 +21,18 @@ let clean (s : string) : string =
              " ")
          else (
            let c_str : string = String.make 1 c in
-           (* if String.contains "\"" c
-              then "\\\""
-              else *)
-           match String.equal " " c_str, !writing_space with
-           | true, true -> ""
-           | true, false ->
-             writing_space := true;
-             c_str
-           | false, true ->
-             writing_space := false;
-             c_str
-           | false, false -> c_str)))
+           if String.contains "\"" c
+           then "'"
+           else (
+             match String.equal " " c_str, !writing_space with
+             | true, true -> ""
+             | true, false ->
+               writing_space := true;
+               c_str
+             | false, true ->
+               writing_space := false;
+               c_str
+             | false, false -> c_str))))
     ""
     s
 ;;
@@ -94,9 +94,9 @@ let get_filename (f : filename_kind) (is_complete : bool option) : string =
 type filetype_kind = JSON of unit
 
 let build_filename
-      (filename : filename_kind)
-      (filetype : filetype_kind)
-      (is_complete : bool option)
+  (filename : filename_kind)
+  (filetype : filetype_kind)
+  (is_complete : bool option)
   : string
   =
   match filetype with
@@ -104,10 +104,10 @@ let build_filename
 ;;
 
 let build_filepath
-      (output_dir : output_dir_kind)
-      (filename : filename_kind)
-      (filetype : filetype_kind)
-      (is_complete : bool option)
+  (output_dir : output_dir_kind)
+  (filename : filename_kind)
+  (filetype : filetype_kind)
+  (is_complete : bool option)
   : string
   =
   match output_dir, filetype with
@@ -121,8 +121,7 @@ let build_filepath
     Filename.concat s (build_filename filename filetype is_complete)
 ;;
 
-(** https://discuss.ocaml.org/t/how-to-create-a-new-file-while-automatically-creating-any-intermediate-directories/14837/5
-*)
+(** https://discuss.ocaml.org/t/how-to-create-a-new-file-while-automatically-creating-any-intermediate-directories/14837/5 *)
 let rec create_parent_dir (fn : string) =
   let parent_dir = Filename.dirname fn in
   if not (Sys.file_exists parent_dir)
@@ -134,69 +133,47 @@ let rec create_parent_dir (fn : string) =
 type dumpable_kind = LTS of Lts.lts
 (* | FSM of Fsm.fsm *)
 
-(* let handle_filecontents
-   (filename : string)
-   (filetype : filetype_kind)
-   (to_dump : dumpable_kind)
-   : string * bool option
-   =
-   match to_dump, filetype with
-   | LTS s, JSON () -> JSON.LTS.lts filename s, Utils.is_complete s.info
+(* let handle_filecontents (filename : string) (filetype : filetype_kind)
+   (to_dump : dumpable_kind) : string * bool option = match to_dump, filetype
+   with | LTS s, JSON () -> JSON.LTS.lts filename s, Utils.is_complete s.info
    ;; *)
 
 type json_action_name = string
 type json_action_silent = bool
 type json_action_annotations = string list
 
-module JSON_Alphabet = Set.Make (struct
-    type t = json_action_name * (json_action_silent * json_action_annotations)
+type json_action =
+  json_action_name * (json_action_silent * json_action_annotations)
 
-    let compare a b =
-      match String.compare (fst a) (fst b) with
-      | 0 ->
-        (match
-           ( Bool.compare (fst (snd a)) (fst (snd b))
-           , List.compare
-               (fun a' b' -> String.compare a' b')
-               (snd (snd a))
-               (snd (snd b)) )
-         with
-         | 0, 0 -> 0
-         | 0, n -> n
-         | n, _ -> n)
-      | n -> n
-    ;;
-  end)
+(* module JSON_Alphabet = Set.Make (struct type t = json_action_name *
+   (json_action_silent * json_action_annotations)
+
+   let compare a b = match String.compare (fst a) (fst b) with | 0 -> (match (
+   Bool.compare (fst (snd a)) (fst (snd b)) , List.compare (fun a' b' ->
+   String.compare a' b') (snd (snd a)) (snd (snd b)) ) with | 0, 0 -> 0 | 0, n
+   -> n | n, _ -> n) | n -> n ;; end) *)
 
 type json_state_name = string
 type json_state_info = string
+type json_state = json_state_name * json_state_info
 
-module JSON_States = Set.Make (struct
-    type t = json_state_name * json_state_info
+(* module JSON_States = Set.Make (struct type t = json_state
 
-    let compare a b =
-      match String.compare (fst a) (fst b) with
-      | 0 -> String.compare (snd a) (snd b)
-      | n -> n
-    ;;
-  end)
+   let compare a b = match String.compare (fst a) (fst b) with | 0 ->
+   String.compare (snd a) (snd b) | n -> n ;; end) *)
 
-module JSON_Edges = Set.Make (struct
-    type t = (json_state_name * json_state_name) * (string * string)
+type json_edge_info = string
 
-    let compare a b =
-      let c1 = String.compare (fst (fst a)) (fst (fst b)) in
-      let c2 = String.compare (snd (fst a)) (snd (fst b)) in
-      let c3 = String.compare (fst (snd a)) (fst (snd b)) in
-      let c4 = String.compare (snd (snd a)) (snd (snd b)) in
-      match c1, c2, c3, c4 with
-      | 0, 0, 0, 0 -> 0
-      | 0, 0, 0, n -> n
-      | 0, 0, n, _ -> n
-      | 0, n, _, _ -> n
-      | n, _, _, _ -> n
-    ;;
-  end)
+type json_edge =
+  (json_state_name * json_state_name) * (json_action_name * json_edge_info)
+
+(* module JSON_Edges = Set.Make (struct type t = json_edge
+
+   let compare a b = let c1 = String.compare (fst (fst a)) (fst (fst b)) in let
+   c2 = String.compare (snd (fst a)) (snd (fst b)) in let c3 = String.compare
+   (fst (snd a)) (fst (snd b)) in let c4 = String.compare (snd (snd a)) (snd
+   (snd b)) in match c1, c2, c3, c4 with | 0, 0, 0, 0 -> 0 | 0, 0, 0, n -> n |
+   0, 0, n, _ -> n | 0, n, _, _ -> n | n, _, _, _ -> n ;; end) *)
 
 type model_info =
   { name : string
@@ -206,10 +183,10 @@ type model_info =
 
 type json_model =
   { info : model_info
-  ; alphabet : JSON_Alphabet.t
+  ; alphabet : json_action Queue.t
   ; initial_state : json_state_name
-  ; state_list : JSON_States.t
-  ; edge_list : JSON_Edges.t
+  ; state_list : json_state Queue.t
+  ; edge_list : json_edge Queue.t
   }
 
 let string_opt (s : string option) : string =
@@ -225,23 +202,25 @@ let to_json_model (filename : string) (model : dumpable_kind) : json_model =
   | LTS s ->
     let extra = s.info in
     let info = { name = filename; kind = "lts"; extra } in
-    let alphabet = JSON_Alphabet.empty in
+    let alphabet = Queue.create () in
     let initial_state = string_opt s.init in
     let state_list =
       Lts.States.fold
-        (fun (state : Lts.state) (acc : JSON_States.t) ->
-          JSON_States.add (state.name, string_opt state.info) acc)
+        (fun (state : Lts.state) (acc : json_state Queue.t) ->
+          Queue.push (state.name, string_opt state.info) acc;
+          acc)
         s.states
-        JSON_States.empty
+        (Queue.create ())
     in
     let edge_list =
       Lts.Transitions.fold
-        (fun (edge : Lts.transition) (acc : JSON_Edges.t) ->
-          JSON_Edges.add
+        (fun (edge : Lts.transition) (acc : json_edge Queue.t) ->
+          Queue.push
             ((edge.from, edge.destination), (edge.label, string_opt edge.info))
-            acc)
+            acc;
+          acc)
         s.transitions
-        JSON_Edges.empty
+        (Queue.create ())
     in
     { info; alphabet; initial_state; state_list; edge_list }
 ;;
@@ -271,21 +250,26 @@ let write_json_extra_to_file (oc : out_channel) (i : Utils.model_info option)
 ;;
 
 let write_json_info_to_file (oc : out_channel) (i : model_info) : unit =
-  Printf.fprintf oc (* open  info *) "\t\"info\": {\n";
+  Printf.fprintf oc (* open info *) "\t\"info\": {\n";
   Printf.fprintf oc "\t\t\"name\": \"%s\",\n" i.name;
   Printf.fprintf oc "\t\t\"kind\": \"%s\",\n" i.kind;
   write_json_extra_to_file oc i.extra;
   Printf.fprintf oc (* close info *) "\t},\n"
 ;;
 
-let write_json_alphabet_to_file (oc : out_channel) (i : JSON_Alphabet.t) : unit =
-  if JSON_Alphabet.is_empty i
+let write_json_alphabet_to_file (oc : out_channel) (i : json_action Queue.t)
+  : unit
+  =
+  if Queue.is_empty i
   then Printf.fprintf oc "\t\"alphabet\": [],\n"
   else (
-    let is_first = ref true in
     Printf.fprintf oc "\t\"alphabet\": [";
-    JSON_Alphabet.iter
-      (fun (action_name, (action_silent, action_annotations)) ->
+    let is_first = ref true in
+    let rec iterate (n : int) () : unit =
+      match n with
+      | 0 -> ()
+      | _ ->
+        let action_name, (action_silent, action_annotations) = Queue.pop i in
         Printf.fprintf oc "%s" (handle_if_first is_first);
         Printf.fprintf oc "\t\t{\n";
         Printf.fprintf oc "\t\t\t\"action\": \"%s\",\n" action_name;
@@ -301,62 +285,113 @@ let write_json_alphabet_to_file (oc : out_channel) (i : JSON_Alphabet.t) : unit 
               Printf.fprintf oc "\t\t\t\t\"%s\"" action_annotation)
             action_annotations;
           Printf.fprintf oc "\t\t\t],\n");
-        Printf.fprintf oc "\t\t}")
-      i;
+        Printf.fprintf oc "\t\t}";
+        iterate (n - 1) ()
+    in
+    iterate (Queue.length i) ();
     Printf.fprintf oc "\n\t],\n")
 ;;
 
-let write_json_states_to_file (oc : out_channel) (i : JSON_States.t) : unit =
-  if JSON_States.is_empty i
+let write_xl_string_to_file (oc : out_channel) (xlstr : string) : unit =
+  if String.length xlstr > 80
+  then (
+    let _, smaller_strings =
+      String.fold_left
+        (fun (build_str, acc) s ->
+          if List.length build_str > 80
+          then (
+            Queue.push (String.of_seq (List.to_seq build_str)) acc;
+            [ s ], acc)
+          else s :: build_str, acc)
+        ([], Queue.create ())
+        xlstr
+    in
+    let rec iterate (n : int) () : unit =
+      match n with
+      | 0 -> ()
+      | _ ->
+        let s = Queue.pop smaller_strings in
+        Printf.fprintf oc "%s" s;
+        iterate (n - 1) ()
+    in
+    iterate (Queue.length smaller_strings) ())
+  else Printf.fprintf oc "%s" xlstr
+;;
+
+let write_json_states_to_file (oc : out_channel) (i : json_state Queue.t) : unit
+  =
+  if Queue.is_empty i
   then Printf.fprintf oc "\t\"states\": [],\n"
   else (
-    let is_first = ref true in
     Printf.fprintf oc "\t\"states\": [";
-    JSON_States.iter
-      (fun (state_name, state_info) ->
+    let is_first = ref true in
+    let rec iterate (n : int) () : unit =
+      match n with
+      | 0 -> ()
+      | _ ->
+        let state_name, state_info = Queue.pop i in
         Printf.fprintf oc "%s" (handle_if_first is_first);
         Printf.fprintf oc "\t\t{\n";
         Printf.fprintf oc "\t\t\t\"name\": \"%s\",\n" state_name;
-        Printf.fprintf oc "\t\t\t\"info\": %s\n" state_info;
-        Printf.fprintf oc "\t\t}")
-      i;
+        (* Printf.fprintf oc "\t\t\t\"info\": %s\n" state_info; *)
+        Printf.fprintf oc "\t\t\t\"info\": ";
+        write_xl_string_to_file oc state_info;
+        Printf.fprintf oc "\n";
+        Printf.fprintf oc "\t\t}";
+        iterate (n - 1) ()
+    in
+    iterate (Queue.length i) ();
     Printf.fprintf oc "\n\t]\n")
 ;;
 
-let write_json_edges_to_file (oc : out_channel) (i : JSON_Edges.t) : unit =
-  if JSON_Edges.is_empty i
+let write_json_edges_to_file (oc : out_channel) (i : json_edge Queue.t) : unit =
+  if Queue.is_empty i
   then Printf.fprintf oc "\t\"edges\": [],\n"
   else (
-    let is_first = ref true in
     Printf.fprintf oc "\t\"edges\": [";
-    JSON_Edges.iter
-      (fun ((from_state, dest_state), (action_label, action_info)) ->
+    let is_first = ref true in
+    let rec iterate (n : int) () : unit =
+      match n with
+      | 0 -> ()
+      | _ ->
+        let (from_state, dest_state), (action_label, action_info) =
+          Queue.pop i
+        in
         Printf.fprintf oc "%s" (handle_if_first is_first);
         Printf.fprintf oc "\t\t{\n";
         Printf.fprintf oc "\t\t\t\"from\": \"%s\",\n" from_state;
         Printf.fprintf oc "\t\t\t\"dest\": \"%s\",\n" dest_state;
         Printf.fprintf oc "\t\t\t\"labl\": \"%s\",\n" action_label;
         Printf.fprintf oc "\t\t\t\"info\": %s\n" action_info;
-        Printf.fprintf oc "\t\t}")
-      i;
+        Printf.fprintf oc "\t\t}";
+        iterate (n - 1) ()
+    in
+    iterate (Queue.length i) ();
     Printf.fprintf oc "\n\t],\n")
 ;;
 
-let write_json_to_file (oc : out_channel) (m : json_model) : unit =
-  Printf.fprintf (* open  json *) oc "{\n";
+let write_json_to_file (m : json_model) (filepath : string) : unit =
+  let oc = open_out filepath in
+  Printf.fprintf (* open json *) oc "{\n";
   write_json_info_to_file oc m.info;
+  close_out oc;
+  let oc = open_out filepath in
   write_json_alphabet_to_file oc m.alphabet;
-  Printf.fprintf oc "\t\"initial_state\": %s\n," m.initial_state;
+  close_out oc;
+  let oc = open_out filepath in
+  Printf.fprintf oc "\t\"initial_state\": %s,\n" m.initial_state;
   write_json_edges_to_file oc m.edge_list;
   write_json_states_to_file oc m.state_list;
-  Printf.fprintf (* close json *) oc "}\n"
+  let oc = open_out filepath in
+  Printf.fprintf (* close json *) oc "}\n";
+  close_out oc
 ;;
 
 let write_to_file
-      (output_dir : output_dir_kind)
-      (filename : filename_kind)
-      (filetype : filetype_kind)
-      (model : dumpable_kind)
+  (output_dir : output_dir_kind)
+  (filename : filename_kind)
+  (filetype : filetype_kind)
+  (model : dumpable_kind)
   : string
   =
   (* convert model to json *)
@@ -367,10 +402,8 @@ let write_to_file
   (* check parent directory exists *)
   create_parent_dir filepath;
   (* write to file *)
-  let oc = open_out filepath in
   (* List.iter (fun (line : string) -> Printf.fprintf oc "%s" line) lines; *)
-  write_json_to_file oc json;
-  close_out oc;
+  write_json_to_file json filepath;
   (* return filepath *)
   filepath
 ;;
--- src/dump_to_file.mli
+++ src/dump_to_file.mli
@@ -1,5 +1,6 @@
 val perm : int
 val default_output_dir : string
+val clean : string -> string
 
 type output_dir_kind =
   | Default of unit
@@ -34,181 +35,42 @@ type json_action_name = string
 type json_action_silent = bool
 type json_action_annotations = json_action_name list
 
-module JSON_Alphabet : sig
-  type elt = json_action_name * (json_action_silent * json_action_annotations)
-  type t
-
-  val empty : t
-  val add : elt -> t -> t
-  val singleton : elt -> t
-  val remove : elt -> t -> t
-  val union : t -> t -> t
-  val inter : t -> t -> t
-  val disjoint : t -> t -> json_action_silent
-  val diff : t -> t -> t
-  val cardinal : t -> int
-  val elements : t -> elt list
-  val min_elt : t -> elt
-  val min_elt_opt : t -> elt option
-  val max_elt : t -> elt
-  val max_elt_opt : t -> elt option
-  val choose : t -> elt
-  val choose_opt : t -> elt option
-  val find : elt -> t -> elt
-  val find_opt : elt -> t -> elt option
-  val find_first : (elt -> json_action_silent) -> t -> elt
-  val find_first_opt : (elt -> json_action_silent) -> t -> elt option
-  val find_last : (elt -> json_action_silent) -> t -> elt
-  val find_last_opt : (elt -> json_action_silent) -> t -> elt option
-  val iter : (elt -> unit) -> t -> unit
-  val fold : (elt -> 'acc -> 'acc) -> t -> 'acc -> 'acc
-  val map : (elt -> elt) -> t -> t
-  val filter : (elt -> json_action_silent) -> t -> t
-  val filter_map : (elt -> elt option) -> t -> t
-  val partition : (elt -> json_action_silent) -> t -> t * t
-  val split : elt -> t -> t * json_action_silent * t
-  val is_empty : t -> json_action_silent
-  val mem : elt -> t -> json_action_silent
-  val equal : t -> t -> json_action_silent
-  val compare : t -> t -> int
-  val subset : t -> t -> json_action_silent
-  val for_all : (elt -> json_action_silent) -> t -> json_action_silent
-  val exists : (elt -> json_action_silent) -> t -> json_action_silent
-  val to_list : t -> elt list
-  val of_list : elt list -> t
-  val to_seq_from : elt -> t -> elt Seq.t
-  val to_seq : t -> elt Seq.t
-  val to_rev_seq : t -> elt Seq.t
-  val add_seq : elt Seq.t -> t -> t
-  val of_seq : elt Seq.t -> t
-end
+type json_action =
+  json_action_name * (json_action_silent * json_action_annotations)
 
 type json_state_name = json_action_name
 type json_state_info = json_state_name
+type json_state = json_state_info * json_state_info
+type json_edge_info = json_state_info
 
-module JSON_States : sig
-  type elt = json_state_info * json_state_info
-  type t
-
-  val empty : t
-  val add : elt -> t -> t
-  val singleton : elt -> t
-  val remove : elt -> t -> t
-  val union : t -> t -> t
-  val inter : t -> t -> t
-  val disjoint : t -> t -> json_action_silent
-  val diff : t -> t -> t
-  val cardinal : t -> int
-  val elements : t -> elt list
-  val min_elt : t -> elt
-  val min_elt_opt : t -> elt option
-  val max_elt : t -> elt
-  val max_elt_opt : t -> elt option
-  val choose : t -> elt
-  val choose_opt : t -> elt option
-  val find : elt -> t -> elt
-  val find_opt : elt -> t -> elt option
-  val find_first : (elt -> json_action_silent) -> t -> elt
-  val find_first_opt : (elt -> json_action_silent) -> t -> elt option
-  val find_last : (elt -> json_action_silent) -> t -> elt
-  val find_last_opt : (elt -> json_action_silent) -> t -> elt option
-  val iter : (elt -> unit) -> t -> unit
-  val fold : (elt -> 'acc -> 'acc) -> t -> 'acc -> 'acc
-  val map : (elt -> elt) -> t -> t
-  val filter : (elt -> json_action_silent) -> t -> t
-  val filter_map : (elt -> elt option) -> t -> t
-  val partition : (elt -> json_action_silent) -> t -> t * t
-  val split : elt -> t -> t * json_action_silent * t
-  val is_empty : t -> json_action_silent
-  val mem : elt -> t -> json_action_silent
-  val equal : t -> t -> json_action_silent
-  val compare : t -> t -> int
-  val subset : t -> t -> json_action_silent
-  val for_all : (elt -> json_action_silent) -> t -> json_action_silent
-  val exists : (elt -> json_action_silent) -> t -> json_action_silent
-  val to_list : t -> elt list
-  val of_list : elt list -> t
-  val to_seq_from : elt -> t -> elt Seq.t
-  val to_seq : t -> elt Seq.t
-  val to_rev_seq : t -> elt Seq.t
-  val add_seq : elt Seq.t -> t -> t
-  val of_seq : elt Seq.t -> t
-end
-
-module JSON_Edges : sig
-  type elt =
-    (json_state_info * json_state_info) * (json_state_info * json_state_info)
-
-  type t
-
-  val empty : t
-  val add : elt -> t -> t
-  val singleton : elt -> t
-  val remove : elt -> t -> t
-  val union : t -> t -> t
-  val inter : t -> t -> t
-  val disjoint : t -> t -> json_action_silent
-  val diff : t -> t -> t
-  val cardinal : t -> int
-  val elements : t -> elt list
-  val min_elt : t -> elt
-  val min_elt_opt : t -> elt option
-  val max_elt : t -> elt
-  val max_elt_opt : t -> elt option
-  val choose : t -> elt
-  val choose_opt : t -> elt option
-  val find : elt -> t -> elt
-  val find_opt : elt -> t -> elt option
-  val find_first : (elt -> json_action_silent) -> t -> elt
-  val find_first_opt : (elt -> json_action_silent) -> t -> elt option
-  val find_last : (elt -> json_action_silent) -> t -> elt
-  val find_last_opt : (elt -> json_action_silent) -> t -> elt option
-  val iter : (elt -> unit) -> t -> unit
-  val fold : (elt -> 'acc -> 'acc) -> t -> 'acc -> 'acc
-  val map : (elt -> elt) -> t -> t
-  val filter : (elt -> json_action_silent) -> t -> t
-  val filter_map : (elt -> elt option) -> t -> t
-  val partition : (elt -> json_action_silent) -> t -> t * t
-  val split : elt -> t -> t * json_action_silent * t
-  val is_empty : t -> json_action_silent
-  val mem : elt -> t -> json_action_silent
-  val equal : t -> t -> json_action_silent
-  val compare : t -> t -> int
-  val subset : t -> t -> json_action_silent
-  val for_all : (elt -> json_action_silent) -> t -> json_action_silent
-  val exists : (elt -> json_action_silent) -> t -> json_action_silent
-  val to_list : t -> elt list
-  val of_list : elt list -> t
-  val to_seq_from : elt -> t -> elt Seq.t
-  val to_seq : t -> elt Seq.t
-  val to_rev_seq : t -> elt Seq.t
-  val add_seq : elt Seq.t -> t -> t
-  val of_seq : elt Seq.t -> t
-end
+type json_edge =
+  (json_edge_info * json_edge_info) * (json_edge_info * json_edge_info)
 
 type model_info =
-  { name : json_state_info
-  ; kind : json_state_info
+  { name : json_edge_info
+  ; kind : json_edge_info
   ; extra : Utils.model_info option
   }
 
 type json_model =
   { info : model_info
-  ; alphabet : JSON_Alphabet.t
-  ; initial_state : json_state_info
-  ; state_list : JSON_States.t
-  ; edge_list : JSON_Edges.t
+  ; alphabet : json_action Queue.t
+  ; initial_state : json_action_name
+  ; state_list : json_state Queue.t
+  ; edge_list : json_edge Queue.t
   }
 
 val string_opt : string option -> string
 val is_model_complete : json_model -> bool option
 val to_json_model : string -> dumpable_kind -> json_model
+val handle_if_first : bool ref -> string
 val write_json_extra_to_file : out_channel -> Utils.model_info option -> unit
 val write_json_info_to_file : out_channel -> model_info -> unit
-val write_json_alphabet_to_file : out_channel -> JSON_Alphabet.t -> unit
-val write_json_states_to_file : out_channel -> JSON_States.t -> unit
-val write_json_edges_to_file : out_channel -> JSON_Edges.t -> unit
-val write_json_to_file : out_channel -> json_model -> unit
+val write_json_alphabet_to_file : out_channel -> json_action Queue.t -> unit
+val write_xl_string_to_file : out_channel -> string -> unit
+val write_json_states_to_file : out_channel -> json_state Queue.t -> unit
+val write_json_edges_to_file : out_channel -> json_edge Queue.t -> unit
+val write_json_to_file : json_model -> string -> unit
 
 val write_to_file
   :  output_dir_kind
