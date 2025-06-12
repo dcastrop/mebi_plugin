b85d4c7bb6ba4e1352823836459ff8da576198c2
Jonah Pears <pearsandcabbage@gmail.com>
2025-06-12 10:55:19 +0100

    finished extended vernac for bisim, merge and minim algorithms

--- src/vernac.ml
+++ src/vernac.ml
@@ -10,13 +10,17 @@ type result_kind =
   | Merge of (Fsm.fsm * Fsm.fsm * Fsm.fsm)
   | Bisim of Bisimilarity.result
 
+(** [fail_if_incomplete * bounds * initial_term] *)
 type term_params = bool * int * Constrexpr.constr_expr
+
+(** [term_params * primary_lts] *)
+type term_lts_params = (term_params * Libnames.qualid)
 
 type run_kind =
   | LTS of term_params
   | FSM of term_params
   | Minim of term_params
-  | Merge of (term_params * term_params)
-  | Bisim of (term_params * term_params)
+  | Merge of (term_lts_params * term_lts_params) 
+  | Bisim of (term_lts_params * term_lts_params) 
 
 type run_params = run_kind * Libnames.qualid list
--- src/vernac.mli
+++ src/vernac.mli
@@ -11,12 +11,13 @@ type result_kind =
   | Bisim of Bisimilarity.result
 
 type term_params = bool * int * Constrexpr.constr_expr
+type term_lts_params = term_params * Libnames.qualid
 
 type run_kind =
   | LTS of term_params
   | FSM of term_params
   | Minim of term_params
-  | Merge of (term_params * term_params)
-  | Bisim of (term_params * term_params)
+  | Merge of (term_lts_params * term_lts_params)
+  | Bisim of (term_lts_params * term_lts_params)
 
 type run_params = run_kind * Libnames.qualid list
