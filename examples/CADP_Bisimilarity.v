bc4b3ff964ecf99b920a80750ec604588ead344a
Jonah Pears <pearsandcabbage@gmail.com>
2025-07-03 16:34:43 +0100

    stuck on clos rt t

--- examples/CADP_Bisimilarity.v
+++ examples/CADP_Bisimilarity.v
@@ -3,6 +3,7 @@ Require Import MEBI.Examples.CADP.
 (* https://rocq-prover.org/doc/v8.9/stdlib/Coq.Relations.Relation_Operators.html *)
 Require Import Relation_Definitions.
 Require Import Relation_Operators.
+Require Import Operators_Properties.
 (* Variable R1 : relation M. *)
 
 Set Primitive Projections.
@@ -11,7 +12,7 @@ Section Definitions.
   Context (M : Type) (A : Type).
   Definition LTS : Type := M -> option A -> M -> Prop.
   (* tau-labelled transition *)
-  Definition tau (R : LTS) : relation M := fun x y => R x None y.
+  Definition tau (R : LTS) : relation M := fun (x y : M) => R x None y.
 End Definitions.
 Arguments tau {M A} R.
 
@@ -21,6 +22,62 @@ Section WeakTrans.
   (* trace of tau-labelled transitions *)
   Definition silent : relation M := clos_refl_trans_1n M (tau lts).
   Definition silent1 : relation M := clos_trans_1n M (tau lts).
+
+  Print clos_refl_trans_1n.
+
+  (* Lemma rt_from_t : 
+    forall R m m',
+    inclusion (clos_refl_trans_1n R m m') (clos_trans_1n R m m'). *)
+  
+  Lemma clos_rt_from_t : forall x y z,
+    clos_trans_1n M (tau lts) x y ->
+    clos_trans_1n M (tau lts) x z ->
+
+  Print clos_rt_t.
+  About clos_rt_t.
+  Check clos_rt_t.
+  (* Lemma clos_rt_t_1n : forall x y z, 
+    clos_refl_trans_1n M (tau lts) x y -> 
+    clos_trans_1n M (tau lts) y z -> 
+    clos_trans_1n M (tau lts) x z.
+  Proof.
+    intros x y z Hxy Hyz.
+    constructor.
+    match z with
+    | y => reflexivity
+    | _ => 
+    end. *)
+  
+  (* Lemma rt_from_t : 
+    forall m m',
+    clos_trans_1n M (tau lts) m m' -> 
+    clos_refl_trans_1n M (tau lts) m m'.
+  Proof.
+    intros m m' H.
+    Print clos_rt_t.
+    About clos_rt_t.
+    Check clos_rt_t.
+    apply clos_rt_t.
+    apply (@clos_rt_t M (tau lts) m m' _).
+    Print clos_trans_1n.
+    Print clos_refl_trans_1n.
+    Print clos_rt_t. *)
+    (* constructor. *)
+
+
+    (* intros m m'. *)
+    (* destruct clos_trans_1n. *)
+    (* destruct clos_refl_trans_1n. *)
+    (* constructor. *)
+
+
+
+  (* Lemma silent_from_silent1 : forall m m', silent1 m m' -> silent m m'.
+  Proof.
+    intros m m' H. *)
+
+    
+    
 
   (*  x ==> pre_str ->^a post_str ==> y *)
   Record weak_tr (x z : M) a (t y : M) : Prop :=
@@ -38,7 +95,7 @@ Section WeakSim.
           weak ltsM m1 (Some a) m2 ->
           exists n2, weak ltsN n1 (Some a) n2 /\ G m2 n2;
         sim_tau : forall m2,
-          (*  weak ltsM m1 None m2 -> *)
+          (* weak ltsM m1 None m2 -> *)
           silent1 ltsM m1 m2 ->
           exists n2, silent ltsN n1 n2 /\ G m2 n2;
       }.
@@ -53,6 +110,80 @@ Section WeakBisim.
   Definition weak_bisim (s : M) (t : N) : Prop
     := weak_sim ltsM ltsN s t /\ weak_sim ltsN ltsM t s.
 End WeakBisim.
+
+
+Check tau.
+About tau.
+(* Lemma silent_refl (M : Type) : forall m m', 
+  clos_refl_trans_1n M (fun x y => R x None y) m m' -> *)
+
+
+Lemma weak_sim_refl (M A : Type) (ltsM : LTS M A) (m : M)
+  : weak_sim ltsM ltsM m m.
+Proof.
+  revert m.
+  cofix CH.
+  intros m.
+  constructor 1.
+  split.
+  - intros m' a Hm.
+    exists m'.
+    split.
+    + apply Hm.
+    + apply CH. Guarded.
+  - intros m' Hm. 
+    exists m'.
+    (* unfold silent1 in Hm. *)
+    split. 
+    + 
+      Print clos_rt_t.
+      Check clos_rt_t.
+      About clos_rt_t.
+      (* apply (@clos_rt_t M (silent ltsM) m m'). *)
+      
+      
+      Check silent1.
+      Check silent.
+      About silent1.
+      About silent.
+      (* exact (tau ltsM m m'). *)
+      (* constructor. *)
+      unfold silent.
+      Print clos_refl_trans_1n.
+
+      (* unfold silent1 in Hm. *)
+      (* apply clos_rt_t in Hm. *)
+      (* induction . *)
+
+      (* apply tau with (y:=m). *)
+      (* unfold tau. *)
+      constructor.
+
+      (* Show Proof. *)
+      (* match goal with 
+      | [ x ] => constructor
+      end. *)
+
+      Check tau.
+      (* replace y with m'. *)
+      (* exists y. *)
+      constructor.
+      Check clos_refl_trans_1n.
+      About clos_refl_trans_1n.
+      constructor.
+    apply Hm.   
+
+  
+  (* eapply simF. *)
+  simpl.
+  About simF.
+  unfold simF.
+  intros m' a tr.
+  exists m'.
+  split.
+  - exact tr.
+  - apply CH. Guarded.
+Qed.
 
 Module BisimTest1.
   Inductive action : Type := | TheAction1 | TheAction2.
@@ -104,6 +235,29 @@ Module BisimTest1.
         termLTS (tfix t) a t'
     .
 
+
+  Ltac example_tactic CH :=
+   let r := fresh "r" in
+   let a := fresh "a" in
+   let tr := fresh "tr" in
+   constructor; intros r a tr;
+
+   (*  *)
+   repeat
+     match goal with
+     | [ H : termLTS ?l ?a ?r |- _ ] =>
+         inversion_clear H; simpl in *
+     end;
+   (*  *)
+
+   eexists;
+   split; [ repeat constructor | idtac ];
+   try apply sim_refl; try apply CH.
+
+  Ltac example_bisim :=
+    let CH := fresh "CH" in
+    cofix CH; repeat (example_tactic CH).
+
   About weak_tr.
   About Pack_weak.
   About weak.
@@ -116,16 +270,56 @@ Module BisimTest1.
   Example test1_n := (tact TheAction1 (tfix (tact TheAction1 trec))).
 
   Goal weak_sim termLTS termLTS test1_m test1_n.    
-  Proof.    
+  Proof. example_bisim. Qed.
+
+  
+  Goal weak_sim termLTS termLTS test1_m test1_n.    
+  Proof. 
+    unfold test1_m.
+    unfold test1_n.
     cofix CH.
     constructor.
     split.
-    About weak_sim.
-    About simF.
-    - intros m' a Hm. 
-      inversion_clear Hm. 
-      inversion H. 
-      exists m'. 
+    (* About weak_sim. *)
+    (* About simF. *)
+    - intros m' a Hm.
+      eexists.
+      constructor.
+      + unfold weak.
+        eexists.
+        eexists.
+        constructor 1.
+        * constructor 1.
+        * 
+          (* eapply do_act1.  *)
+          constructor 1. 
+      
+      
+      (* inversion_clear Hm; subst. *)
+      (* inversion H; subst. *)
+      exists n'. 
+      split.
+      + unfold weak.
+        About weak_tr.
+        eexists.
+        eexists.
+        constructor.
+        * constructor.
+        * 
+          eapply do_act1. 
+          constructor. 
+
+        (* intros post_m pre_m'. *)
+        (* exists test1_m. *)
+        (* exists m'. *)
+        constructor.
+        * unfold silent. 
+
+
+        (* eexists. *)
+        intros z t.
+        apply do_act1. 
+        unfold test1_n.
       
 
 
@@ -149,26 +343,6 @@ Lemma glued_sim : weak_sim (lts g1 action) bigstep.
 Lemma glued_bisim (g1 g2 : composition) : 
 
 
-(* Lemma weak_sim_refl (M A : Type) (ltsM : LTS M A) (m : M)
-  : weak_sim ltsM ltsM  m m.
-Proof.
-  revert m.
-  cofix CH.
-  intros m.
-  constructor.
-  
-  constructor.
-  
-  (* eapply simF. *)
-  simpl.
-  About simF.
-  unfold simF.
-  intros m' a tr.
-  exists m'.
-  split.
-  - exact tr.
-  - apply CH. Guarded.
-Qed. *)
 
 
 
