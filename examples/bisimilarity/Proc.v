ee464e459e8819318375fcc15571234a315ad296
thecathe <pearsandcabbage@gmail.com>
2025-08-29 15:08:58 +0100

    updated actions, crafting ltac that handles multiple cases

--- examples/bisimilarity/Proc.v
+++ examples/bisimilarity/Proc.v
@@ -4,36 +4,176 @@ Require Coq.Program.Tactics.
 Require Import MEBI.Bisimilarity.
 Require Import MEBI.Examples.Proc.
 
-Inductive bLTS : term -> option label -> term -> Prop :=
-| BISIM_WRAP : forall t1 t2 a, termLTS t1 a t2 -> bLTS t1 a t2.
+(****************************************************************************)
+(* NOTE: makes goals easier to read, e.g., [H : tend = t] -> [H : t = tend] *)
+Ltac ltr_goals :=
+  repeat match goal with
+  | [ t : term, H : tend       = ?t |- _ ] => symmetry in H
+  | [ t : term, H : trec       = ?t |- _ ] => symmetry in H
+  | [ t : term, H : tfix _     = ?t |- _ ] => symmetry in H
+  | [ t : term, H : tact _ _   = ?t |- _ ] => symmetry in H
+  | [ t : term, H : tseq _ _   = ?t |- _ ] => symmetry in H
+  | [ t : term, H : tpar _ _   = ?t |- _ ] => symmetry in H
+  | [ t : term, H : tsubst _ _ = ?t |- _ ] => symmetry in H
+  | |- _  => idtac
+  end.
 
-Lemma bLTS_unwrap : forall x1 x2 a, bLTS x1 a x2 -> termLTS x1 a x2.
-Proof. intros x1 x2 a Ht. inversion Ht; subst. apply H. Qed.
+(* NOTE: computes certain fun, e.g., [tsubst _ _] for recursive unfoldings *)
+Ltac compute_goals :=
+  repeat match goal with
+  | [ t : term, H : ?t = tsubst _ _ |- _ ] => compute in H
+  | |- _ => idtac
+  end.
 
-(* TODO *)
-Module Test2.
-  Example x : term := tfix (tseq (tpar (tact ASend tend) 
-                                       (tact ARecv tend)) trec).
+(* NOTE: reformats all goals in a consistent way, resolves certain fun calls *)
+Ltac fmt_goals := ltr_goals; compute_goals.
 
-  Example y : term := tfix (tseq (tpar (tact ARecv tend) 
-                                       (tact ASend tend)) 
-                                 (tseq (tpar (tact ASend tend) 
-                                             (tact ARecv tend)) trec)).
+(****************************************************************************)
+(* NOTE: *)
 
-  (* Ltac sim_silent1_refl :=
-    match goal with 
-    | [ Hx : ?tx = _
-      , Hy : ?ty = ?vy 
-      , CH : @weak_sim _ _ _ _ _ ?tx ?ty 
-      |- 
-        forall nx, @silent1 _ _ _ ?tx nx -> 
-          exists ny, @silent _ _ _ ?ty ny /\ 
-            @weak_sim _ _ _ _ _ nx ny
-      ] => 
-        intros _x Htx; apply blts_silent1_refl in Htx;
-        rewrite <- Htx; exists vy; rewrite <- Hy; 
-        split; [ constructor | apply CH ]
-    end. *)
+Ltac wsim_case_weak_action_filter_from_silent :=
+  let xa := fresh "xa0" in let xb := fresh "xb0" in 
+  let xc := fresh "xc0" in let xd := fresh "xd0" in 
+  let Hpre_tau := fresh "Hpre_tau0" in let Hpre_cft := fresh "Hpre_cft0" in 
+  let Hstr := fresh "Hstr0" in let Hpost := fresh "Hpost0" in 
+  inversion Htx as [xa [xd [[| xb xc Hpre_tau Hpre_cft] Hstr Hpost]]]; 
+  [ destruct Hpost; inversion Hstr | ].
+
+Ltac wsim_case_weak_action_filter :=
+  match goal with
+  (* do_fix *)
+  | [ Hx : ?cx = tfix _, Htx : @weak _ _ _ ?cx (Some ?a) _ 
+    |- exists ny, @weak _ _ _ ?cy (Some ?a) ny /\ _ ] => wsim_case_weak_action_filter_from_silent
+    
+  (* do_seq_end*)
+  | [ Hx : ?cx = tseq tend _, Htx : @weak _ _ _ ?cx (Some ?a) _ 
+    |- exists ny, @weak _ _ _ ?cy (Some ?a) ny /\ _ ] => wsim_case_weak_action_filter_from_silent
+    
+  (* do_par_end *)
+  | [ Hx : ?cx = tpar tend  _, Htx : @weak _ _ _ ?cx (Some ?a) _ 
+    |- exists ny, @weak _ _ _ ?cy (Some ?a) ny /\ _ ] => wsim_case_weak_action_filter_from_silent
+  
+  | |- _ => idtac
+  end.
+
+Ltac wsim_case_weak_action := 
+  let nx := fresh "x0" in intros nx;
+  let a := fresh "a0" in intros a;
+  let Htx := fresh "Htx0" in intros Htx;
+  match goal with
+  | [ Hx : ?cx = ?vx, Hy : ?cy = ?vy
+    , Htx : @weak _ _ _ ?cx (Some a) nx 
+    |- exists ny, @weak _ _ _ ?cy (Some ?a) ny /\ @weak_sim _ _ _ _ _ nx ny ] => 
+      rewrite Hy; rewrite Hx in Htx; 
+      first [ wsim_case_weak_action_filter | idtac ]
+  end.
+
+(****************************************************************************)
+(* NOTE: *)
+
+Ltac wsim_case_silent_action := idtac.
+
+(****************************************************************************)
+(* NOTE: called at the beginning of each new proof that terms are wsim. *)
+Ltac wsim_new_cofix :=
+  let CH := fresh "CH0" in cofix CH; 
+  apply In_sim, Pack_sim; [ wsim_case_weak_action | wsim_case_silent_action ].
+
+(* NOTE: entrypoint, handles first cofix before [wsim_new_cofix]. *)
+Ltac wsim_pre_cofix_unfold :=
+  let x := fresh "x0" in intros x;
+  let y := fresh "y0" in intros y;
+  let Hx := fresh "Hx0" in intros Hx;
+  let Hy := fresh "Hy0" in intros Hy;
+  match goal with 
+  | [ Hx : ?x = ?vx, Hy : ?y = ?vy 
+    |- @weak_sim _ _ _ _ _ ?x ?y ] => unfold vx, vy in Hx, Hy; wsim_new_cofix
+  end.
+
+Tactic Notation "solve_wsim" := wsim_pre_cofix_unfold.
+
+(****************************************************************************)
+Section Test2.
+  Example p : term := tfix (tseq (tpar (tact (send A) tend) 
+                                       (tact (recv A) tend)) trec).
+
+  Example q : term := tfix (tseq (tpar (tact (recv A) tend) 
+                                       (tact (send A) tend)) 
+                                 (tseq (tpar (tact (send A) tend) 
+                                             (tact (recv A) tend)) trec)).
+  
+  Example r : term := tseq (tpar (tact (recv A) tend) 
+                                 (tact (send A) tend)) 
+                           (tfix (tseq (tpar (tact (send A) tend) 
+                                             (tact (recv A) tend)) trec)).
+
+(****************************************************************************)
+  
+  (* NOTE: quick checks to see if current ltac breaks any of the other cases *)
+  Example wsim_test_pq : forall x y, x = p -> y = q -> weak_sim termLTS termLTS x y. Proof. solve_wsim. Admitted.
+  Example wsim_test_pr : forall x y, x = p -> y = r -> weak_sim termLTS termLTS x y. Proof. solve_wsim. Admitted.
+  Example wsim_test_qp : forall x y, x = q -> y = q -> weak_sim termLTS termLTS x y. Proof. solve_wsim. Admitted.
+  Example wsim_test_qr : forall x y, x = q -> y = r -> weak_sim termLTS termLTS x y. Proof. solve_wsim. Admitted.
+  Example wsim_test_rp : forall x y, x = r -> y = p -> weak_sim termLTS termLTS x y. Proof. solve_wsim. Admitted.
+  Example wsim_test_rq : forall x y, x = r -> y = q -> weak_sim termLTS termLTS x y. Proof. solve_wsim. Admitted.
+
+  (* NOTE: below is the "hands-on" proof for testing *)
+  Example wsim_testing : forall x y,
+    x = p -> y = q ->
+    (* x = p -> y = r -> *)
+    (* x = q -> y = p -> *)
+    (* x = q -> y = r -> *)
+    (* x = r -> y = p -> *)
+    (* x = r -> y = q -> *)
+    weak_sim termLTS termLTS x y.
+  Proof.
+    solve_wsim.
+     
+     
+    {
+      (* inversion Htx0 as [?xa [?xb [Hpre Hstr Hpost]]]. *)
+      (* inversion Htx0 as [?xa [?xd [[| ?xb ?xc Hpre_tau Hpre_cft] Hstr Hpost]]]. *)
+      (* inversion Htx0 as [?xa [?xd [Hpre Hstr Hpost]]]. *)
+      inversion Hpre_tau0; fmt_goals. rewrite H1 in Hpre_cft0.
+      inversion Hpre_cft0; fmt_goals. 
+      {
+        rewrite H in Hstr0.
+        inversion Hstr0; fmt_goals.
+        inversion H6; fmt_goals. rewrite H7 in H5. rewrite H5 in Hstr0.
+      }
+
+      destruct Hpost; inversion Hstr; fmt_goals. 
+      admit.
+      admit.
+      admit.
+      admit.
+
+
+    }
+
+                                       
+(****************************************************************************)
+
+  Theorem wsim_xy : forall x y, x = p -> y = q -> weak_sim termLTS termLTS x y.
+  Proof. 
+  Admitted.
+
+  Theorem wsim_yx : forall x y, x = q -> y = p -> weak_sim termLTS termLTS x y.
+  Admitted.
+
+  Theorem wbisim_xy 
+  : forall x y, x = p -> y = q -> weak_bisim termLTS termLTS x y.
+  Proof.
+    intros x0 y0 Hx0 Hy0; unfold x, y in Hx0, Hy0. 
+    rewrite Hx0, Hy0; unfold weak_bisim. 
+    split; [ apply wsim_xy | apply wsim_yx ]; trivial.
+  Qed.
+End Test2.
+
+(****************************************************************************)
+(****************************************************************************)
+(****************************************************************************)
+(****************************************************************************)
 
   Ltac handle_weak_transition :=
     match goal with 
@@ -125,25 +265,6 @@ Module Test2.
 
     end. *)
 
-  (* NOTE: makes goals easier to read e.g., [H : tend = t] -> [H : t = tend] *)
-  Ltac ltr_sym :=
-    repeat match goal with
-    | [ t : term, H : tend = ?t |- _ ] => symmetry in H
-    | [ t : term, H : trec = ?t |- _ ] => symmetry in H
-    | [ t : term, H : tfix _ = ?t |- _ ] => symmetry in H
-    | [ t : term, H : tsubst _ _ = ?t |- _ ] => compute in H
-    | [ t : term, H : tact _ _ = ?t |- _ ] => symmetry in H
-    | [ t : term, H : tseq _ _ = ?t |- _ ] => symmetry in H
-    | [ t : term, H : tpar _ _ = ?t |- _ ] => symmetry in H
-    | [ |- _ ] => idtac
-    end.
-
-  Ltac compute_tsubst :=
-    ltr_sym;
-    repeat match goal with
-    | [ t : term, H : tsubst _ _ = ?t |- _ ] => compute in H
-    | [ |- _ ] => idtac
-    end.
 
   (* NOTE: for silent actions that occur within a weak visible action *)
   Ltac wsim_weak_silent_action :=
@@ -205,25 +326,6 @@ Module Test2.
         rewrite Hy
         ; rewrite Hx in Htx
     end.
-
-  (* NOTE: called at the beginning of each new proof that terms are wsim. *)
-  Ltac wsim_cofix :=
-    let CH := fresh "CH" in cofix CH; 
-    apply In_sim, Pack_sim; [ wsim_weak | wsim_silent ].
-
-  (* NOTE: entrypoint, handles first cofix before [wsim_cofix]. *)
-  Ltac wsim_pre_cofix_unfold :=
-    intros;
-    match goal with 
-    | [ _Hx : ?tx = ?vx
-      , _Hy : ?ty = ?vy |- @weak_sim _ _ _ _ _ ?tx ?ty 
-      ] => 
-        let Hx := fresh "Hx" in rename _Hx into Hx;
-        let Hy := fresh "Hy" in rename _Hy into Hy;
-        unfold vx, vy in Hx, Hy; wsim_cofix
-    end.
-
-  Tactic Notation "solve_wsim" := wsim_pre_cofix_unfold.
 
   Theorem ws_xy : forall x0 y0, x0 = x -> y0 = y -> weak_sim bLTS bLTS x0 y0.
   Proof. 
