e7dab2321433bf84f9408ebd1068743f47403624
David Castro-Perez <david.castro.dcp@gmail.com>
2025-09-12 16:33:34 +0100

    Slight changes to proofs

--- theories/Bisimilarity.v
+++ theories/Bisimilarity.v
@@ -75,7 +75,7 @@ Hint Resolve sim_weak : rel_db.
 Lemma weak_sim_refl {M A} (lts : LTS M A) : forall x, weak_sim lts lts x x.
 Proof.
   cofix CH; intros; repeat constructor; intros; exists m2.
-  split; info_eauto with rel_db.
+  split; eauto with rel_db.
 Qed.
 Hint Resolve weak_sim_refl : rel_db.
 
@@ -88,7 +88,7 @@ Proof.
   apply out_sim in H1 as [H1]. 
   specialize (H1 _ _ H) as [n2 [W Ws]].
   apply Ih in Ws as [n3 [St Ws]]. 
-  inversion W. info_eauto with rel_db.
+  inversion W. eauto with rel_db.
 Qed.
 (* Hint Resolve weak_sim_silent_clos : rel_db. *)
 
@@ -97,7 +97,7 @@ Lemma weak_sim_act_clos : forall {M N A ltsM ltsN m1 n1},
     forall m2 a, weak ltsM m1 m2 a ->
                  exists n2, weak ltsN n1 n2 a /\ weak_sim ltsM ltsN m2 n2.
 Proof.
-  intros. destruct H0 as [a m1' m2' PRE ACT POST|TAUs]; eauto with rel_db.
+  intros. destruct H0 as [a m1' m2' PRE ACT POST|TAUs].
   - apply (weak_sim_silent_clos H) in PRE. destruct PRE as [n2 [S1 W1]].
     destruct (sim_weak (out_sim W1) ACT) as [n3 [WA2 W2]]. 
     apply (weak_sim_silent_clos W2) in POST. destruct POST as [n4 [S2 W3]].
@@ -118,7 +118,7 @@ Proof.
   apply out_sim in H; destruct H as [H].
   specialize (H _ _ H1); destruct H as [n2 [W Ws]].
   eapply weak_sim_act_clos in H0; eauto. destruct H0 as [n3 [W' Ws']].
-  exists n3. split; eauto with rel_db.
+  eauto with rel_db.
 Qed.
 Hint Resolve weak_sim_trans : rel_db.
 
@@ -131,12 +131,12 @@ End WeakBisim.
 Hint Unfold weak_bisim : rel_db.
 
 Lemma wk_bisim_refl {M A} (lts : LTS M A) : forall x, weak_bisim lts lts x x.
-Proof. info_eauto with rel_db. Qed.
+Proof. eauto with rel_db. Qed.
 
 Lemma wk_bisim_trans {M A} (lts : LTS M A) : forall x y z,
     weak_bisim lts lts x y -> weak_bisim lts lts y z -> weak_bisim lts lts x z.
-Proof. intros ??? [] []; info_eauto with rel_db. Qed.
+Proof. intros ??? [] []; eauto with rel_db. Qed.
 
 Lemma wk_bisim_sym {M A} (lts : LTS M A) : forall x y,
     weak_bisim lts lts x y -> weak_bisim lts lts y x.
-Proof. intros ?? []; info_eauto with rel_db. Qed.
+Proof. intros ?? []; eauto with rel_db. Qed.
