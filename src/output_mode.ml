ce65979d61291de1b611ad508399b7f42930beeb
thecathe <pearsandcabbage@gmail.com>
2026-01-05 15:45:21 +0000

    non-debug output messages will no longer show function call

--- src/output_mode.ml
+++ src/output_mode.ml
@@ -79,7 +79,9 @@ module Rocq : OUTPUT_MODE = Make (struct
       : level -> string -> unit
       =
       let open Pp in
-      let f (g : ?loc:Loc.t -> Pp.t -> unit) : string -> unit =
+      let f ?(__FUNCTION__ : string = "") (g : ?loc:Loc.t -> Pp.t -> unit)
+        : string -> unit
+        =
         fun (y : string) ->
         let a : Pp.t =
           (match __FUNCTION__ with "" -> mt () | z -> str z ++ str ": ")
@@ -93,11 +95,11 @@ module Rocq : OUTPUT_MODE = Make (struct
         g (seq [ v 0 (seq [ a; b ]); ws 0; c ] |> hv 0)
       in
       function
-      | Debug -> f Feedback.msg_debug
+      | Debug -> f ~__FUNCTION__ Feedback.msg_debug
       | Info -> f Feedback.msg_info
       | Notice -> f Feedback.msg_notice
-      | Warning -> f Feedback.msg_warning
-      | Error -> f Feedback.msg_debug
+      | Warning -> f ~__FUNCTION__ Feedback.msg_warning
+      | Error -> f ~__FUNCTION__ Feedback.msg_debug
     ;;
 
     let special_output_fun
