1645a2737ed932e13e72ec70185845fed20157ed
Jonah Pears <pearsandcabbage@gmail.com>
2026-01-22 10:18:54 +0000

    disabled logs

--- src/rocq_bindings.ml
+++ src/rocq_bindings.ml
@@ -1,8 +1,8 @@
 (***********************************************************************)
 module Log : Logger.LOGGER_TYPE = Logger.MkDefault ()
 
-let () = Log.Config.configure_output Debug true
-let () = Log.Config.configure_output Trace true
+let () = Log.Config.configure_output Debug false
+let () = Log.Config.configure_output Trace false
 (***********************************************************************)
 
 module C : Hashtbl.S with type key = Constr.t = Hashtbl.Make (struct
