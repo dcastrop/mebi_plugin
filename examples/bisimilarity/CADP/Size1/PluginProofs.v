9657b97ff9a2c0a14a9da32d3daa61a28f8b212d
Jonah Pears <pearsandcabbage@gmail.com>
2026-03-05 15:49:44 +0000

    pulled property test from paper, needs work

--- examples/bisimilarity/CADP/Size1/PluginProofs.v
+++ examples/bisimilarity/CADP/Size1/PluginProofs.v
@@ -30,3 +30,32 @@ Example wsim_lts_bigstep : weak_sim lts bigstep c1 c1.
 Proof. MeBi Sim Begin lts c1 And bigstep c1 Using step. 
   (* Iteration History: 395 <- _ <- _ <- _  *) 
   MeBi Sim Solve 395. Qed.
+
+(****************************)
+
+(* TODO: just copied from paper *)
+(* Inductive act' : Type := | ENTER : nat -> nat -> label | LEAVE : nat -> nat -> label. *)
+
+Inductive spec_state : Type :=
+| Free : spec_state
+| Held : nat -> spec_state
+.
+
+(* NOTE: needed to add pid *)
+Inductive spec_lts : spec_state -> option label -> spec_state -> Prop :=
+| SVC_ENTER : forall i, spec_lts Free (Some (ENTER, i)) (Held i)
+| SVC_LEAVE : forall i, spec_lts (Held i) (Some (LEAVE, i)) Free
+.
+
+MeBi Divider "Examples.Bisimilarity.CADP.Size1.PluginProofs.bigstep_spec_lts".
+(* Example wsim_bigstep_spec_lts : weak_sim bigstep spec_lts c1 Free. 
+Proof. MeBi Sim Begin bigstep c1 And spec_lts Free Using step.
+  (* Iteration History: _ <- _ <- _ <- _ *) 
+  MeBi Sim Solve 1000. Qed. *)
+
+  
+MeBi Divider "Examples.Bisimilarity.CADP.Size1.PluginProofs.spec_lts_bigstep".
+(* Example wsim_spec_lts_bigstep : weak_sim spec_lts bigstep Free c1. 
+Proof. MeBi Sim Begin spec_lts Free And bigstep c1 Using step. 
+  (* Iteration History: _ <- _ <- _ <- _  *) 
+  MeBi Sim Solve 1000. Qed. *)
